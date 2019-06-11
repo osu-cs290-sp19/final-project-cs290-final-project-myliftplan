24a7adfcd529f4b14c2344f454be485939d2b553
toatui23 <seumalon@flip3.engr.oregonstate.edu>
2019-06-11 16:53:30 -0700

    working on more mongo

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-document.getElementById("addLift").addEventListener("click", function(){
+function addLift() {
 	var table = document.getElementsByClassName("liftTable");
 	var row = table[0].insertRow(-1);
 	var cell1 = row.insertCell(0);
@@ -16,7 +16,7 @@ document.getElementById("addLift").addEventListener("click", function(){
 	var cell6 = document.createElement('button');
 	button.innerHTML = "Delete";
 
-});
+}
 
 
 function deleteRow(btn) {
@@ -24,11 +24,59 @@ function deleteRow(btn) {
   row.parentNode.removeChild(row);
 }
 
+function getPersonIdFromURL() {
+	var path = window.location.pathname;
+  	var pathParts = path.split('/');
+  	if (pathParts[1] === "plans") {
+    	return pathParts[2];
+ 	} else {
+    	return null;
+  	}
+}
 function handleSaveClick() {
+	var i;
+	var j;
+	var cellVals = new Array(5);
+	var table = document.getElementById("lift-table");
+
 	
+
+	for(i = 1; i < table.rows.length; i++) {
+		var cells = table.rows.item(i).cells;
+
+		for(j = 0; j < cells.length - 1; j++)
+		{
+			cellVals[j] = cells.item(j).innerHTML;
+		}
+	}
+	
+	var postRequest = new XMLHttpRequest();
+    var requestURL = '/plans/' + getPersonIdFromURL()  + '/editPlan';
+	postRequest.open('POST', requestURL);
+
+	var requestBody = JSON.stringify({
+		muscle: cellVals[0],
+		liftName: cellVals[1],
+		sets: cellVals[2],
+		reps: cellVals[3],
+		rest: cellVals[4]
+	});
+
+	postRequest.setRequestHeader('Content-Type', 'application/json');
+	postRequest.send(requestBody);
 }
 
-var saveWorkoutButton = documentgetElementById('save-button');
-if(saveWorkoutButton){
+ 
+window.addEventListener('DOMContentLoaded', function() {
+
+	var saveWorkoutButton = document.getElementById("save-button");
+	if(saveWorkoutButton){
 	saveWorkoutButton.addEventListener('click', handleSaveClick);
-}
+	}
+
+	var addLiftRow = document.getElementById("addLift");
+	if(addLiftRow) {
+		addLiftRow.addEventListener('click', addLift);
+	}
+
+});
